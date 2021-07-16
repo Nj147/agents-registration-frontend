248431d5647ae47c1bd20be65521d2842a698d3e
“QAcpardeep” <cpardeep@qa.com>
2021-07-16 09:11:28 +0100

    added in hints

--- test/uk/gov/hmrc/agentsregfrontend/controllers/BusinessNameControllerSpec.scala
+++ test/uk/gov/hmrc/agentsregfrontend/controllers/BusinessNameControllerSpec.scala
@@ -26,66 +26,62 @@ import play.api.http.Status._
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{charset, contentAsString, contentType, defaultAwaitTimeout, redirectLocation, session, status}
-import uk.gov.hmrc.agentsregfrontend.models.Address
 
 class BusinessNameControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
 
   override def fakeApplication(): Application =
-      new GuiceApplicationBuilder()
-        .configure(
-          "metrics.jvm" -> false,
-          "metrics.enabled" -> false
-        )
-        .build()
+    new GuiceApplicationBuilder()
+      .configure(
+        "metrics.jvm" -> false,
+        "metrics.enabled" -> false
+      )
+      .build()
 
-    private val getfakeRequest = FakeRequest("GET", "/")
-    private val postfakeRequest = FakeRequest("POST", "/")
+  private val getfakeRequest = FakeRequest("GET", "/")
+  private val postfakeRequest = FakeRequest("POST", "/")
+  private val controller = app.injector.instanceOf[BusinessNameController]
 
-    private val testAddress = Address("1 New Address", "A01 2BC")
-
-    private val controller = app.injector.instanceOf[BusinessNameController]
-
-    "GET /businessName" should {
-      "return 200" in {
-        val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
-        status(result) shouldBe 200
-      }
-      "return HTML" in {
-        val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
-        contentType(result) shouldBe Some("text/html")
-        charset(result)     shouldBe Some("utf-8")
-      }
-      "return a page with 1 input" in {
-        val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
-        Jsoup.parse(contentAsString(result)).getElementsByClass("govuk-input--width-10").size shouldBe 1
-      }
-      "have a pre-populated input field if already entered once" in {
-        val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest.withSession("businessName" -> "Test Business Name"))
-        Jsoup.parse(contentAsString(result)).getElementById("businessName").`val`() shouldBe "Test Business Name"
-      }
-      "redirect if the user is logged in" in {
-        val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest.withSession("arn" -> "ARN0000001"))
-        status(result) shouldBe SEE_OTHER
-      }
+  "GET /businessName" should {
+    "return 200" in {
+      val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
+      status(result) shouldBe 200
     }
-
-    "POST /businessName" should {
-      "redirect to the next form page with added session values if valid values added" in {
-        val result = controller.processBusinessName(isUpdate = false).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> "testBusinessName"))
-        status(result) shouldBe SEE_OTHER
-        session(result).get("businessName") shouldBe Some("testBusinessName")
-        redirectLocation(result) shouldBe Some(s"${routes.EmailController.displayEmailPage(false)}")
-      }
-      "return bad request with nothing in session when form is left empty" in {
-        val result = controller.processBusinessName(isUpdate = false).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> ""))
-        session(result).isEmpty
-        status(result) shouldBe BAD_REQUEST
-      }
-      "redirect to the summary page with added session values if updating and valid values added" in {
-        val result = controller.processBusinessName(isUpdate = true).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> "testBusinessName"))
-        status(result) shouldBe Status.SEE_OTHER
-        session(result).get("businessName") shouldBe Some("testBusinessName")
-        redirectLocation(result) shouldBe Some(s"${routes.SummaryController.summary()}")
-      }
+    "return HTML" in {
+      val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+    "return a page with 1 input" in {
+      val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest)
+      Jsoup.parse(contentAsString(result)).getElementsByClass("govuk-input--width-10").size shouldBe 1
+    }
+    "have a pre-populated input field if already entered once" in {
+      val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest.withSession("businessName" -> "Test Business Name"))
+      Jsoup.parse(contentAsString(result)).getElementById("businessName").`val`() shouldBe "Test Business Name"
+    }
+    "redirect if the user is logged in" in {
+      val result = controller.displayBusinessNamePage(isUpdate = false).apply(getfakeRequest.withSession("arn" -> "ARN0000001"))
+      status(result) shouldBe SEE_OTHER
     }
   }
+
+  "POST /businessName" should {
+    "redirect to the next form page with added session values if valid values added" in {
+      val result = controller.processBusinessName(isUpdate = false).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> "testBusinessName"))
+      status(result) shouldBe SEE_OTHER
+      session(result).get("businessName") shouldBe Some("testBusinessName")
+      redirectLocation(result) shouldBe Some(s"${routes.EmailController.displayEmailPage(false)}")
+    }
+    "return bad request with nothing in session when form is left empty" in {
+      val result = controller.processBusinessName(isUpdate = false).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> ""))
+      session(result).isEmpty
+      status(result) shouldBe BAD_REQUEST
+    }
+    "redirect to the summary page with added session values if updating and valid values added" in {
+      val result = controller.processBusinessName(isUpdate = true).apply(postfakeRequest.withFormUrlEncodedBody("businessName" -> "testBusinessName"))
+      status(result) shouldBe Status.SEE_OTHER
+      session(result).get("businessName") shouldBe Some("testBusinessName")
+      redirectLocation(result) shouldBe Some(s"${routes.SummaryController.summary()}")
+    }
+  }
+}
